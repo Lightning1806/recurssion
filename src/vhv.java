b8aa87a29271040aca2e06d47491211ef27e2681
Vishal Aynodkar <vishal@localhost.localdomain>
2023-03-12 16:27:54 +0530

    s

---
+++ src/vhv.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class vhv {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        System.out.println("Rohit Dande");
+        Scanner in = new Scanner(System.in);
+        int a = new int
+        )
+    }
+}
